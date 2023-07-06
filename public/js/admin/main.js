df7b88c25db2a31ef8b0085f05f28483ea3230f6
dinhhb <habuudinh@gmail.com>
2023-07-07 02:17:02 +0700

    adjust layout

--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -2,6 +2,12 @@ const deleteDish = (btn) => {
     const dishId = btn.parentNode.querySelector('[name=dishId]').value;
     const dishElement = btn.closest('tr');
 
+    // Show confirmation dialog
+    const confirmDelete = confirm("Bạn có chắc chắn muốn xoá món ăn này không?");
+    if (!confirmDelete) {
+        return; // Do nothing if the user cancels the deletion
+    }
+
     fetch('/admin/dish/' + dishId, {
         method: 'DELETE'
     }).then(result => {
